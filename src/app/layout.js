04a1254cd724168ea54e75b91a338280b2dd432b
durjoy1516 <durjoy0184@gmail.com>
2026-06-22 14:41:24 +0600

    Update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Tiles Gallery",
   description: "Discover your perfect aesthetic",
+  icons: {
+    icon: "/vector.png",
+  },
 };
 
 export default function RootLayout({ children }) {
